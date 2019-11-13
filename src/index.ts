bf1b8455cacbbb0cde15917185a007f3cb4a57f4
brunodev09 <bruno.mayol.giannotti@gmail.com>
2019-11-13 19:46:40 -0300

    adiçao d3e porta por env

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import Server from "./app";
 import UserController from "./controllers/user";
 import AuthenticationController from "./controllers/auth";
 import PostController from "./controllers/post";
-
-const server = new Server([new UserController(), new AuthenticationController(), new PostController()], 5000);
+//@ts-ignore
+const server = new Server([new UserController(), new AuthenticationController(), new PostController()], process.env.PORT | 5000);
 server.listen();
