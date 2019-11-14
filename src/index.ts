c8b1a8f74e2674add891c3a597e4bc463587b391
brunodev09 <bruno.mayol.giannotti@gmail.com>
2019-11-14 18:14:26 -0300

    Enviromental variables.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import { resolve } from "path";
+import { config } from "dotenv";
+config({ path: resolve(__dirname, "../.env") })
+
 import Server from "./app";
 import UserController from "./controllers/user";
 import AuthenticationController from "./controllers/auth";
